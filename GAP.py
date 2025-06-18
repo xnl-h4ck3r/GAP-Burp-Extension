30ef18b73f76d51c8121f6dc8ad47f0a0dafcce2
/XNL-h4ck3r <xnl@xnl-h4ck3r.uk>
2025-06-18 02:25:12 +0100

    v5.8 - see CHANGELOG.md

--- GAP.py
+++ GAP.py
@@ -8,7 +8,7 @@ Get full instructions at https://github.com/xnl-h4ck3r/GAP-Burp-Extension/blob/m
 
 Good luck and good hunting! If you really love the tool (or any others), or they helped you find an awesome bounty, consider BUYING ME A COFFEE! (https://ko-fi.com/xnlh4ck3r) (I could use the caffeine!)
 """
-VERSION="5.6"
+VERSION="5.8"
 
 _debug = False
 
